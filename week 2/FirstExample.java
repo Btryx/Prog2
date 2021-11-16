f2602dedef184cdf95ac211d2370d4cae9e04031
Btryx <beatryx15@gmail.com>
2021-11-16 12:34:02 +0100

    remov

--- week 2/FirstExample.java
+++
@@ -1,7 +0,0 @@
-class FirstExample {
-
-	public static void main(String[] args) {
-
-		System.out.println("Hello Java!");
-	}
-}
--- week 5/p1/Demo.java
+++
@@ -1,8 +0,0 @@
-package p1;
-public class Demo{
-	public static void main(String[] args) {
-		Protection protObj = new Protection();
-		Derived derObj = new Derived();
-		SamePackage samepacObj = new SamePackage();
-	}
-}
